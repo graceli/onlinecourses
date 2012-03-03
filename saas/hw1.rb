5b06b18ee4d62cf4fd4ec18f05c923cfdf432d18
Grace Li <fullofgrace88@gmail.com>
2012-03-03 16:19:07 -0500

    saas: question 1

---
+++ saas/hw1.rb
@@ -0,0 +1,39 @@
+
+def palindrome?(string)
+	# ignore case, punctuation, and nonword characters
+	# set all letters in the string to lower case
+	# reverse the string
+	# return true if two strings are equal otherwise false
+
+	word_string = string.gsub(/\W/, '').downcase
+  return word_string == word_string.reverse
+end
+
+
+def count_words(string)
+  count = {}
+  string.downcase.scan(/\b(\w+)\b/).each do |key|
+    if count.has_key?(key)
+      count[key] += 1
+    else
+      count[key] = 1
+    end
+  end
+  
+  return count
+end
+
+# Tests
+
+# palindrome?("A man, a plan, a canal -- Panama") # => true
+# palindrome?("Madam, I'm Adam!") #=> true
+# palindrome?("Abracadabra") #=> false
+
+# puts palindrome?("A man, a plan, a canal -- Panama")
+# puts palindrome?("Madam, I'm Adam!")
+# puts palindrome?("Abracadabra")
+
+
+# puts count_words "Doo bee doo bee doo"
+
+
